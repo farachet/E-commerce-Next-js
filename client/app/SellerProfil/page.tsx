e74a21225608cdfbba7acf61c679670e06c23256
samarzribi <samarzribi014@gmail.com>
2023-07-21 01:35:15 +0100

    working on cruds

--- client/app/SellerProfil/page.tsx
+++ client/app/SellerProfil/page.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Container,Box, Typography } from '@mui/material'
 import style from "./page.module.css";
 import Profil from './profil/page';
-import Products from './products/page'
+import Products from './products/page';
+import AddProducts from "./addProducts/page"
 
-type Props = {}
 
-const profilSeller = () => {
+const profilSeller  : React.FC = () => {
   return (
    <Box  style={{  background: 'linear-gradient(123deg, #984D38 0%, #181E41 63%)' }} >
     <Profil />
+    <Box  style={{display : "flex" , marginTop : 50}} >
+    <AddProducts />
     <Products/>
-    
+   
+    </Box>
     </Box>
  
 
