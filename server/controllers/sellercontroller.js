2af1e57c825fb12fc49089ddd40200726e058905
samarzribi <samarzribi014@gmail.com>
2023-07-22 10:21:34 +0100

    working on seller profil

--- server/controllers/sellercontroller.js
+++ server/controllers/sellercontroller.js
@@ -2,5 +2,17 @@ const {seller}= require('../database/models/seller')
 module.exports= {
     AddSeller:(req,res)=>{
         seller.create({})
-    }
+    },
+
+    updateSeller:(req,res)=>{
+        console.log(req.params.sellerId)
+        seller.update(req.body.firstName , req.body.lastName , {where:{id:req.params.id}})
+        .then(result =>
+         res.status(201).json(result)
+         )
+        .catch(err=> 
+        res.status(500).json(err)
+        )
+    },
+
 }
